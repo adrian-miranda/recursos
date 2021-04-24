000b09902b9165cc099c76cc881072b1e89f7aaf
adrian <adrian.miranda.r@gmail.com>
2021-04-24 03:52:29 -0400

    herramientas  js

---
+++ misFunciones.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var saludosHumanos = "En este archivo iran las herramientas logicas que desarrolle a lo largo del tiempo";
+
+function saludando(x){
+    x = saludosHumanos;
+    return x;
+}
+
+var saludoRecepcionado = saludando();
+console.log(saludoRecepcionado);
