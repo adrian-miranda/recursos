9d65ee26a63786f2015bab7d7a45b7901f30cbee
adrian <adrian.miranda.r@gmail.com>
2021-04-29 21:47:10 -0400

    Ciclo de iteracion basico y llamado

--- js/misFunciones.js
+++ js/misFunciones.js
@@ -1,11 +1,22 @@
-'use strict'
+// 'use strict'
 
-var saludosHumanos = "En este archivo iran las herramientas logicas que desarrolle a lo largo del tiempo";
+// var saludosHumanos = "En este archivo iran las herramientas logicas que desarrolle a lo largo del tiempo";
 
-function saludando(x){
-    x = saludosHumanos;
-    return x;
+// function saludando(x){
+//     x = saludosHumanos;
+//     return x;
+// }
+
+// var saludoRecepcionado = saludando();
+// console.log(saludoRecepcionado);
+
+function suma(){
+    var sum = 0;
+    for(var i = 0 ; i <= 1000 ; i++){
+        sum = i++;
+        console.log(i);
+    }
+    return sum;
 }
 
-var saludoRecepcionado = saludando();
-console.log(saludoRecepcionado);
+suma();
